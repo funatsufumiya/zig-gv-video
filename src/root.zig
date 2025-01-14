1c622adb06f41775b1f8fd9f6ba320511d03f235
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-01-14 22:36:26 +0900

    chore: import ezdxt and comment

--- src/root.zig
+++ src/root.zig
@@ -1,7 +1,22 @@
+// Extreme Gpu Friendly Video Format
+//
+// binary file format:
+// 
+// 0: uint32_t width
+// 4: uint32_t height
+// 8: uint32_t frame count
+// 12: float fps
+// 16: uint32_t format (DXT1 = 1, DXT3 = 3, DXT5 = 5, BC7 = 7)
+// 20: uint32_t frame bytes
+// 24: raw frame storage (lz4 compressed)
+// eof - (frame count) * 16: [(uint64_t, uint64_t)..<frame count] (address, size) of lz4, address is zero based from file head
+//
+
 const std = @import("std");
 const testing = @import("testing");
 
 const lz4 = @import("lz4");
+const ezdxt = @import("ezdxt");
 
 pub const GVFormat = enum(u32) {
     DXT1 = 1,
